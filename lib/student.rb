634aad90df3f7bcb6390c5531a980cec26d97828
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-10 23:13:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User 
+  
+  attr_reader :knowledge
+  
+  def initialize(first_name, last_name)
+    @first_name, @last_name = first_name, last_name
+    @knowledge = []
+  end 
+  
+  def learn("")
+    @knowledge << "" 
+  end 
 
 end
